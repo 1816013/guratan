75d0de504ee5a854139251fa8e52984c9caa6cac
kimura <gfhfhfgfhfjgfhfhgf.com>
2020-01-14 18:43:15 +0900

    追加漏れ

---
+++ myGame/Classes/action/CheckCtr.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <cocos2d.h>
+#include "ActionMng.h"
+#include "input/OPRT_state.h"
+#include "input/OPRT_key.h"
+
+struct actModule;
+
+struct CheckKey
+{
+	// ·°“ü—ÍŠm”F
+	bool operator()(cocos2d::Sprite& sp, actModule& module);
+};
+
+struct CheckAI
+{
+	bool operator()(cocos2d::Sprite& sp, actModule& module);
+};
